4dbac177f91d6602340195bd3b82520c88bc2bb0
Sridaran Thoniyil <sri7thon@gmail.com>
2020-05-08 10:48:22 -0400

    Add battery to config, fix spacing

--- config.h
+++ config.h
@@ -3,25 +3,26 @@
 
 #define CONFIG \
 { \
-   /* name            function               cached */\
-    { "",             get_title,             false }, \
-    { "",             get_bar,               false }, \
-    { "OS: ",         get_os,                true  }, \
-    { "Host: ",       get_host,              true  }, \
-    { "Kernel: ",     get_kernel,            true  }, \
-    { "Uptime: ",     get_uptime,            false }, \
+   /* name            function                 cached */\
+    { "",             get_title,               false }, \
+    { "",             get_bar,                 false }, \
+    { "OS: ",         get_os,                  true  }, \
+    { "Host: ",       get_host,                true  }, \
+    { "Kernel: ",     get_kernel,              true  }, \
+    { "Uptime: ",     get_uptime,              false }, \
+    { "Battery: ",    get_battery_percentage,  false }, \
     SPACER \
     { "Packages: ",   get_packages_pacman,   false }, \
     { "Shell: ",      get_shell,             false }, \
     { "Resolution: ", get_resolution,        false }, \
     { "Terminal: ",   get_terminal,          false }, \
     SPACER \
-    { "CPU: ",        get_cpu,               true  }, \
-    { "GPU: ",        get_gpu1,              true  }, \
-    { "Memory: ",     get_memory,            false }, \
+    { "CPU: ",        get_cpu,                 true  }, \
+    { "GPU: ",        get_gpu1,                true  }, \
+    { "Memory: ",     get_memory,              false }, \
     SPACER \
-    { "",             get_colors1,           false }, \
-    { "",             get_colors2,           false }, \
+    { "",             get_colors1,             false }, \
+    { "",             get_colors2,             false }, \
 }
 
 #define CPU_CONFIG \
